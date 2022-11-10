eaa87c1b7a13ae34f0ef4e0cad937a89a846448a
Henok Ayalew <82248556+Henicode@users.noreply.github.com>
2022-11-10 17:56:02 -0500

    Update and rename index.js to web-index.js

---
+++ web-index.js
@@ -0,0 +1,27 @@
+                                                                // web javascript excercise 
+
+// Adding behaviour to website 
+
+alert('message');  // metod displays an alert dialog with the optional specified content and an ok button.
+                    // used when you want information to come through to the user. 
+                    // alert is the function.
+                    // "message" is the message or string you're displaying.
+                    // ; all javascript end in semicolon so browser understands that is the end of the line.  
+                    
+var exampleData = "store data"; // with var statement you can declare a variable that store data 
+                    // and those data can be "strings", numbers, boolean.
+                    // you can also store many variables in a var by seperating them with a comma like example below.
+var exampleData = "store data",
+    exampleName = "henok",
+    exampleAge = 23,
+    exampleStatus = true;
+                    // you can choose to store data in a var or let, those data can be updated or changed at any time. 
+                    // but if you store data in const, it cannot be updated to changed.
+const exapleData = 3.1415;   // const cannot be changed or reassigned just like the number in pi. 
+
+
+typeof("message");   // typeof lets you identify the type of data that is stored in the message section. 
+
+prompt("input question");    // prompt is similar to an alert but it allows for users to input for the question provided in the message.
+
+
