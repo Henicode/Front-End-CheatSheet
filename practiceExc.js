4afdb8f0f152ae1f95ae8e2c4050bbe9f188c49a
Henok Ayalew <82248556+Henicode@users.noreply.github.com>
2022-11-13 18:54:04 -0500

    Create practiceExc.js
    More challenges

---
+++ practiceExc.js
@@ -0,0 +1,54 @@
+// BMI Calculator with function 
+// BMI = weight(kg)/(height*2(m))
+
+  function bmiCalculator(weight,height){
+    var bmi = weight / (height * height);
+
+   return bmi;
+    
+  }
+                  // --------------BREAK-------------------//
+
+// Combining Comparators in an IF/ELSE statment for BMI Calculator 
+// if bmi < 18.5 . output the bmi and state " underweight "
+// if bmi >= 18.5 and <= 24.9 . output the bmi and state " normal weight "
+// if bmi > 24.9 . output the bmi and state " overweight "
+
+function bmiCalculator(weight, height) {
+  var bmi = weight / (height * height);
+  
+  if (bmi >= 18.5 && bmi <= 24.9){
+    return bmi + " normal weight ";
+  }else if(bmi < 18.5){
+    return bmi + " underweight ";
+  }else if(bmi > 24.9){
+    return bmi + " overweight ";
+  }else {
+    return bmi;
+  }
+}
+
+                  // --------------BREAK-------------------//
+
+// Leap Year. create a function that intake the year and output if its leap or not
+// A year is a leap year if it is evenly divisible by 4. EXCEPT if that year is also evenly divisible by 100.. UNLESS that year is also evenly divisible by 400.
+
+function leapYear(year){
+  
+  if (year % 4 === 0){
+    if(year % 100 === 0){
+      if(year % 400 === 0){
+        return "leap";
+      }
+    }else{
+      return "leap";
+    }
+  }else{
+    return "not leap";
+  }
+  
+}  
+  
+  
+  
+
