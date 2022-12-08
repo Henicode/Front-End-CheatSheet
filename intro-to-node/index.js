8e4c0f5a8505ee8bf6e0729657377581c0def7b2
Henok Ayalew <82248556+Henicode@users.noreply.github.com>
2022-12-08 17:13:21 -0500

    intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,16 @@
+
+var supervillains = require('supervillains');
+var superheros = require('superheroes');
+
+var mySuperVillainsName = supervillains.random();
+var mySuperHeroName = superheros.random();
+
+console.log(mySuperVillainsName);
+console.log(mySuperHeroName);
+
+
+// var supervillains = require("supervillains");
+
+// var mySuperVillainsName = supervillains.all;
+
+// console.log(mySuperVillainsName);
